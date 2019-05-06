16e7eae11212ec3c16e6326a7bd63aaede09cccd
Pro Amedeo <proamedeo@MBP-di-Pro.homenet.telecomitalia.it>
2019-05-06 11:04:33 +0200

    .

--- resources/views/rieducazione_funzionale.blade.php
+++ resources/views/rieducazione_funzionale.blade.php
@@ -1,5 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-  
+  <div class="container-fluid">
+    <div class="row header-rieducazione-funzionale">
+      <div class="col-lg-12 col-md-12 col-sm-12 d-flex justify-content-center align-items-center">
+        <h1>RIEDUCAZIONE <span>FUNZIONALE</span></h1>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-4 col-md-12 col-sm-12 img-rieducazione-funzionale">
+
+      </div>
+      <div class="col-lg-8 col-md-12 col-sm-12 txt-rieducazione-funzionale d-flex justify-content-center align-items-center flex-column">
+        <h1>RIEDUCAZIONE FUNZIONALE IN <span>ACQUA</span></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-8 col-md-12 col-sm-12 txt-rieducazione-funzionale d-flex justify-content-center align-items-center flex-column">
+        <h1>RIEDUCAZIONE FUNZIONALE IN <span style="color:#E67D3B;">PALESTRA</span></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      <div class="col-lg-4 col-md-12 col-sm-12 img-rieducazione-funzionale-gym">
+
+      </div>
+    </div>
+  </div>
 @endsection
